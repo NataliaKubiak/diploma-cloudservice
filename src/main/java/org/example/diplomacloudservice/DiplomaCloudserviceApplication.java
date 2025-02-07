c6b212fbd8cc1a613360d5d4fbb3ab56428af8b9
Natalia Kubiak <nataliia.kubiak@gmail.com>
2025-02-07 19:36:24 +0100

    chore: upload automatically generated files

---
+++ src/main/java/org/example/diplomacloudservice/DiplomaCloudserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.diplomacloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaCloudserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaCloudserviceApplication.class, args);
+    }
+
+}
