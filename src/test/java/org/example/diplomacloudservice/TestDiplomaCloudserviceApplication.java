d2e333744b8298f9ffc333c6c68b47e31597913a
Natalia Kubiak <nataliia.kubiak@gmail.com>
2025-02-17 15:53:02 +0100

    feat: add application-test.yaml

--- src/test/java/org/example/diplomacloudservice/TestDiplomaCloudserviceApplication.java
+++ src/test/java/org/example/diplomacloudservice/TestDiplomaCloudserviceApplication.java
@@ -1,11 +1,11 @@
-package org.example.diplomacloudservice;
-
-import org.springframework.boot.SpringApplication;
-
-public class TestDiplomaCloudserviceApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.from(DiplomaCloudserviceApplication::main).with(TestcontainersConfiguration.class).run(args);
-    }
-
-}
+//package org.example.diplomacloudservice;
+//
+//import org.springframework.boot.SpringApplication;
+//
+//public class TestDiplomaCloudserviceApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.from(DiplomaCloudserviceApplication::main).with(TestcontainersConfiguration.class).run(args);
+//    }
+//
+//}
